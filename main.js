a1e3fa6d0212851de58ad568188214b699910bd7
Gerson Guerrero <jairguerrerosilvera@gmail.com>
2019-06-07 22:17:01 -0500

    Creacion de proyecto en electron y un ejemplo simple

---
+++ main.js
@@ -0,0 +1,22 @@
+const electron = require('electron');
+const path = require('path');
+const url = require('url');
+
+const { app, BrowserWindow } = electron;
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({ width: 800, height: 600 })
+  win.loadURL(url.format({
+    pathname: path.join( __dirname, 'src/index.html'),
+    protocol: 'file',
+    slashes: true
+  }))
+}
+
+app.on('ready', createWindow);
+
+
+
+
